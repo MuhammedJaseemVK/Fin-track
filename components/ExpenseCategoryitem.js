52e49f2805c9b58fd274b04f30bb9268b698531e
Jaseem vk <muhammedjaseemvk96@gmail.com>
2023-03-18 10:24:34 +0530

    made expense items as buttons

--- components/ExpenseCategoryitem.js
+++ components/ExpenseCategoryitem.js
@@ -2,13 +2,16 @@ import {currencyFormatter} from "@/lib/utils";
 
 function ExpenseCategoryItem({color,title,amount}){
     return (
-    <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
-        <div className="flex items-center gap-2">
-                <div className="rounded-full w-[25px] h-[25px]" style={{ backgroundColor: color }} />
-            <h4 className="capitalize">{title}</h4>
-        </div>
-        <p>{currencyFormatter(amount)}</p>
-    </div>
+        <button>
+            <div className="flex items-center justify-between px-4 py-4 bg-slate-700 rounded-3xl">
+                <div className="flex items-center gap-2">
+                    <div className="rounded-full w-[25px] h-[25px]" style={{ backgroundColor: color }} />
+                    <h4 className="capitalize">{title}</h4>
+                </div>
+                <p>{currencyFormatter(amount)}</p>
+            </div>
+        </button>
+    
     );
 }
 
